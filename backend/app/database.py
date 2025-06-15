2f70a0653dc7844919dfeaf9abc27857c66b1b8b
Salima Ammar <selimaammar07@gmail.com>
2025-06-15 17:53:23 -0400

    [~] - Fixed crud file to reuse global connection

--- backend/app/database.py
+++ backend/app/database.py
@@ -16,11 +16,3 @@ database = result.path.lstrip("/")   # everything after the "/"
 hostname = result.hostname
 port     = result.port
 
-conn = psycopg2.connect(
-    host=hostname,
-    port=port,
-    database=database,
-    user=username,
-    password=password,
-    sslmode="require"   # Render’s Postgres requires SSL
-)
